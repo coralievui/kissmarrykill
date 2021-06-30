a401a694551d28ad0763d10ad9505f2c246bede6
Katmyr <78119281+Katmyr@users.noreply.github.com>
2021-06-30 05:06:10 +0200

    Create cartoon.js

---
+++ cartoon.js
@@ -0,0 +1,96 @@
+let gens= [
+["Rick", "https://i.pinimg.com/236x/34/fd/54/34fd54b29fedb82e075732e328d1eb89.jpg"],
+["Morty", "https://i.pinimg.com/236x/6b/bb/b8/6bbbb8473690a996984b33240b33157a.jpg"],
+["Charlie", "https://i.pinimg.com/236x/fb/0b/b4/fb0bb418b497a8193423bcc722f0c440.jpg"],
+["Alastor", "https://i.pinimg.com/236x/23/82/7d/23827db39f367a7478a371ded3f1bf72.jpg"],
+["Angel Dust", "https://i.pinimg.com/236x/80/4b/15/804b1535f06208ab53fe2fa9e1551e01.jpg"],
+["Vaggie", "https://i.pinimg.com/564x/90/a4/e4/90a4e4598019f69eeba1ab47631db029.jpg"],
+["Husk", "https://i.pinimg.com/236x/71/48/b5/7148b5249715c27eb81da90e9fa6ad01.jpg"],
+["Cherri Bomb", "https://i.pinimg.com/236x/80/cb/7d/80cb7d7d4c228cf078c7cd112347e55e.jpg"],
+["Cartman", "https://i.pinimg.com/236x/d4/5c/c2/d45cc21bf10c31bdb98f0f1c4d28c7b0.jpg"],
+["Chef", "https://i.pinimg.com/236x/6d/c0/03/6dc003a6e592807878619a8555374b9e.jpg"],
+["Kyle", "https://i.pinimg.com/564x/da/9c/0d/da9c0d3a71fd82d0241ef5a7ed8960c6.jpg"],
+["Kenny", "https://i.pinimg.com/564x/c5/16/20/c51620249df667e7652882dd119a41b1.jpg"],
+["Stan", "https://i.pinimg.com/564x/ae/27/4f/ae274f29fbdd3881eef8706e35a08838.jpg"],
+["Sam", "https://i.pinimg.com/236x/0f/a4/93/0fa493dcad5d00b8e2f13736959a0131.jpg"],
+["Clover", "https://i.pinimg.com/564x/14/55/f5/1455f52e3c9b324270739ed4b0f43b42.jpg"],
+["Alex", "https://i.pinimg.com/564x/cb/51/04/cb5104af4dc5549401c3dcfc289bc089.jpg"],
+["Jerry", "https://i.pinimg.com/236x/64/37/a9/6437a960ebc468eede4faa1efb459ce5.jpg"],
+["Mandy", "https://i.pinimg.com/236x/80/db/b7/80dbb747852017550bbc0de404229eb6.jpg"],
+["Mabel", "https://i.pinimg.com/564x/5e/01/0f/5e010fe204f2e13f22ac46ae1d6272b5.jpg"],
+["Dipper", "https://i.pinimg.com/564x/e3/df/bf/e3dfbf620b43ac5d8289a3cca40a7ef7.jpg"],
+["Oncle Stan", "https://i.pinimg.com/564x/fe/16/1e/fe161e1d14b0509e650f173273ad87b0.jpg"],
+["Bill Cipher", "https://i.pinimg.com/564x/cb/72/91/cb7291031e3de9af88c124ac57ab1171.jpg"],
+["Wendy", "https://i.pinimg.com/564x/f7/a5/aa/f7a5aa8d70e2838ffbe3c34590a789c3.jpg"],
+["Omer", "https://i.pinimg.com/236x/e2/98/0d/e2980d8d3cbf515d102c295f76012d7f.jpg"],
+["Marge", "https://i.pinimg.com/564x/aa/57/de/aa57de0347b5470a6de8e46ca160c948.jpg"],
+["Bart", "https://i.pinimg.com/564x/8d/b6/cb/8db6cb3f7bff3bf8b29a65c0a98aa1ed.jpg"],
+["Lisa", "https://i.pinimg.com/564x/81/dc/a3/81dca3fa5aad979c4d03b6361edd1101.jpg"],
+["Martin", ""],
+["Diana", ""],
+["Sammy", ""],
+["Scooby-Doo", "https://i.pinimg.com/564x/31/ff/03/31ff03408e3b42ea23dc1125511042b5.jpg"],
+["Vera", ""],
+["Fred", ""],
+["Daphne", "https://i.pinimg.com/236x/25/9c/2c/259c2cca862d2637fcc2cddc4621a2f7.jpg"],
+["Titi", "https://i.pinimg.com/236x/b6/1b/b1/b61bb1fb13411d37f01862e8efff9213.jpg"],
+["Grosminet", "https://i.pinimg.com/236x/a6/ad/69/a6ad6981afae9e9b7070a7ffc4cdcffe.jpg"],
+["Bugs Bunny", "https://i.pinimg.com/236x/f7/f5/c8/f7f5c8e6404ad2c36a15122c1a62ac2c.jpg"],
+["Phineas", "https://i.pinimg.com/564x/18/98/07/189807d80a5ddd8a93ddedcb070b4ff5.jpg"],
+["Ferb", "https://i.pinimg.com/564x/75/fe/d5/75fed53ceefb20e2815ef8d3d5178f25.jpg"],
+["Perry", "https://i.pinimg.com/236x/5c/32/c9/5c32c9b0ff6646214b85eddf2a55cf21.jpg"],
+["Dr Doofenshmirtz", "https://i.pinimg.com/564x/7d/2d/c1/7d2dc1817dd2b2bb24d91da8e6518dfd.jpg"],
+["Candice", "https://i.pinimg.com/564x/c1/48/4b/c1484bff683315e62686e2b4cf49850e.jpg"],
+["Vanessa", "https://i.pinimg.com/236x/34/f8/ed/34f8ed4738a25d770e05d83688733c7a.jpg"],
+["Kim Possible", "https://i.pinimg.com/236x/04/65/e9/0465e9807ce396ba6a1abe1eb486a885.jpg"],
+["Jessica Rabbit", "https://i.pinimg.com/236x/78/d7/0a/78d70a682fe329278c84b6b11f2ebb33.jpg"],
+["Bob l'Éponge", "https://i.pinimg.com/236x/2d/07/8f/2d078f5c9809debc299cbdd8e8f25f59.jpg"],
+["Patrick", "https://i.pinimg.com/236x/18/03/2a/18032a74056820ac4b314a11baa308d4.jpg"],
+["Carlos", "https://i.pinimg.com/236x/a7/2a/e3/a72ae38f412b42164df24fd2906503a2.jpg"],
+["Mr Krabs", "https://i.pinimg.com/564x/d2/12/59/d21259c78a8952636e47276085d2dd81.jpg"],
+["Sandy", "https://i.pinimg.com/236x/b2/cf/28/b2cf288afb1414374bfd8d08cf597be0.jpg"],
+["Bloom", "https://i.pinimg.com/564x/bc/71/51/bc7151e9f26c21a0697efa0f07365fb6.jpg"],
+["Stella", "https://i.pinimg.com/564x/0e/11/f7/0e11f71c867a49b791eaff3addc6a54e.jpg"],
+["Musa", "https://i.pinimg.com/564x/3c/4c/d3/3c4cd3a96a8c3a79f8730faa4667e7f1.jpg"],
+["Flora", "https://i.pinimg.com/564x/70/a5/82/70a582f9c99baed5414ca455636a31e7.jpg"],
+["Layla", "https://i.pinimg.com/236x/29/9a/43/299a4398469e2dcd8667195c0b72997e.jpg"],
+["Roxy", "https://i.pinimg.com/564x/71/e5/89/71e589c1ab85c5f6524ca64a4dcbb1ec.jpg"],
+["Techna", "https://i.pinimg.com/236x/b3/e2/f7/b3e2f77f294341229ed70e11c06b6769.jpg"],
+["Marceline", "https://i.pinimg.com/564x/bf/e1/a4/bfe1a4bdf8cd8cb0accfc42585aad450.jpg"],
+["Bubblegum", "https://i.pinimg.com/564x/95/ec/73/95ec73a6dd5220de6d4a6959996507c2.jpg"],
+["Finn", "https://i.pinimg.com/236x/04/4e/46/044e463109ccc789105872b98db89db5.jpg"],
+["Jake", "https://i.pinimg.com/564x/14/85/a6/1485a6f33b1e4751390dd9fb779ff6ee.jpg"],
+["Flame", "https://i.pinimg.com/564x/ae/d4/f1/aed4f1f300973c48566a427f4e8902e6.jpg"],
+["Rainbow Dash", "https://i.pinimg.com/236x/e7/06/de/e706de0a6a26681f78bebfabf35ff49c.jpg"],
+["Apple Jack", "https://i.pinimg.com/236x/05/93/83/0593835fd4969b85796fb85e7cf91b03.jpg"],
+["Rarity", "https://i.pinimg.com/236x/fa/bd/07/fabd07eb001953fd474b6ec4e9832a3a.jpg"],
+["Fluttershy", "https://i.pinimg.com/236x/69/06/c1/6906c1b348916681e9d49e8376d6a286.jpg"],
+["Pinkie Pie", "https://i.pinimg.com/236x/dd/1f/03/dd1f0361d06e01480c9cef02e112b96c.jpg"],
+["Twilight", "https://i.pinimg.com/236x/f0/a6/35/f0a6350667fb93e6caff0ca9ff2a48bb.jpg"],
+["Tom", "https://i.pinimg.com/236x/84/74/22/847422467444334d8b7df7ee308880a8.jpg"],
+["Jerry", "https://i.pinimg.com/236x/2f/3c/36/2f3c36dd5244ce001eae2547fd6d151b.jpg"],
+["Belle", "https://i.pinimg.com/564x/11/bb/ab/11bbab66afc36e3eae102a08831888c9.jpg"],
+["Bulle", "https://i.pinimg.com/564x/f2/57/e1/f257e1af5637c71e2920e01b5786af99.jpg"],
+["Rebelle", "https://i.pinimg.com/564x/8d/6e/81/8d6e81d3140ec88b9543cbb80c0129e3.jpg"],
+]
+    
+function generate() {
+    let person1 = gens[Math.floor(Math.random() * gens.length)];
+    let person2 = gens[Math.floor(Math.random() * gens.length)];
+    let person3 = gens[Math.floor(Math.random() * gens.length)];
+
+    if (person1[0] == person2[0] || person1[0] == person3[0] || person2[0] == person3[0]) {
+        setTimeout(() => {
+            generate();
+        }, 100);
+    } else {
+        document.getElementById('person1-text').innerText = person1[0];
+        document.getElementById('person1-image').src = person1[1];
+        document.getElementById('person2-text').innerText = person2[0];
+        document.getElementById('person2-image').src = person2[1];
+        document.getElementById('person3-text').innerText = person3[0];
+        document.getElementById('person3-image').src = person3[1];
+    }
+}
+
+generate();
