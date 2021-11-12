438d224812f17ad7e79d9f89597b469c67278c29
Katmyr <78119281+Katmyr@users.noreply.github.com>
2021-11-12 20:43:22 +0100

    Add files via upload

---
+++ prenoms.js
@@ -0,0 +1,105 @@
+let gens = [
+    ["Adrien", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Alice", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Anaïs", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Anaëlle", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Andy", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Anna", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Antoine", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Ariana", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Audrey", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Augustin", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Baptiste", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Briac", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Bruno", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Candice", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Catherine", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Chantal", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Chloé", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Clara", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Damien", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Diego", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Dora", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Édouard", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Éléna", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Emma", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Emmanuel(le)", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Gérard", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Gwendolin(e)", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Harry", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Hélène", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Hervé", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Ines", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Ingrid", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Isabelle", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Jean(ne)", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Jennie", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Joseph", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Jules", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Juliette", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Laure", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Léane", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Léna", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Lisa", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Lony", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Lorette", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Lucas", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Lucie", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Maelyne", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Manon", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Maria", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Marie", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Marie-Liesse", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Martin(e)", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Maurice", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Maxime", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Nathan", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Noël(le)", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Océane", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Ornella", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Paul(ine)", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Philippe", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Pierre", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Rémi", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Rodolph", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Sarah", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Selena", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Sylvain", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Sylvie", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Tatiana", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Titouan", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Tom", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Tomas", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Tristan", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Valentin(e)", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Valérie", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Virginie", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Victoria", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Victorine", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Xavier", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Yves", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Yvon", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Zélie", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+    ["Zoé", "https://i.pinimg.com/564x/6f/37/9e/6f379e55e2d1f4ab715b5c565b37ebff.jpg"],
+]
+
+function generate() {
+    let person1 = gens[Math.floor(Math.random() * gens.length)];
+    let person2 = gens[Math.floor(Math.random() * gens.length)];
+    let person3 = gens[Math.floor(Math.random() * gens.length)];
+
+    if (person1[0] == person2[0] || person1[0] == person3[0] || person2[0] == person3[0]) {
+        setTimeout(() => {
+            generate();
+        }, 100);
+    } else {
+        document.getElementById('person1-text').innerText = person1[0];
+        document.getElementById('person1-image').src = person1[1];
+        document.getElementById('person2-text').innerText = person2[0];
+        document.getElementById('person2-image').src = person2[1];
+        document.getElementById('person3-text').innerText = person3[0];
+        document.getElementById('person3-image').src = person3[1];
+    }
+}
+
+generate();
